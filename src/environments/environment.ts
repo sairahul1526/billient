6df9add9be0bab15c78bba5fe74c0f382d6becc2
sairahul1526 <dravid.rahul1526@gmail.com>
2018-06-06 13:28:00 +0530

    remove firebase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBYEzeY-TI0FZz2oABNKL0W5GBRHAl1fh0",
-    authDomain: "billient-ff8da.firebaseapp.com",
-    databaseURL: "https://billient-ff8da.firebaseio.com",
-    projectId: "billient-ff8da",
-    storageBucket: "billient-ff8da.appspot.com",
-    messagingSenderId: "798121423440"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
 
